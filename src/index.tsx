f05a6bcfea3ad1cecd41b4ce48842da0ed0da6b7
macinjoke <shunji.makino@gmail.com>
2018-12-02 23:56:26 +0900

    react + Typescript 最小構成

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const element = document.getElementById('app')
+
+render(<div>hello, react</div>, element)
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.ts', '.tsx', '.js'
+    ]
+  },
+  devServer: {
+    contentBase: 'dist'
+  }
+}
