7cec5e8f5b405e3317a185a769a3168cbf8642eb
macinjoke <shunji.makino@gmail.com>
2019-01-08 19:27:25 +0900

    headers container のconnect のgenerics 取り去り

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
-import { connect, DispatchProp } from 'react-redux'
-import { bindActionCreators } from 'redux'
 import { logout } from '../state/auth/operations'
-import { State as AuthState } from '../state/auth/reducers'
 import { State } from '../types'
 
-interface DispatchProps {
+interface Props {
+  isLogin: boolean
   logout: () => void
 }
-
-type StateProps = AuthState
-
-type Props = DispatchProps & StateProps
 
 class Header extends React.Component<Props> {
   public render() {
--- src/containers/header.tsx
+++ src/containers/header.tsx
@@ -2,16 +2,11 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Header } from '../components'
 import { logout } from '../state/auth/operations'
-import { State as AuthState } from '../state/auth/reducers'
 import { State } from '../types'
 
-interface DispatchProps {
-  logout: () => void
-}
-
-type StateProps = AuthState
-
-export default connect<StateProps, DispatchProps, {}, State>(
-  s => s.auth,
+export default connect(
+  (s: State) => ({
+    isLogin: s.auth.isLogin,
+  }),
   { logout },
 )(Header)
