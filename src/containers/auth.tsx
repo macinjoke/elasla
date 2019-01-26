a71228177588867a5e11761cab17b96cd02a2051
macinjoke <shunji.makino@gmail.com>
2019-01-27 00:57:50 +0900

    Auth Form を material-ui 化 & styled-components を使ってスタイリング

--- src/containers/auth.tsx
+++ src/containers/auth.tsx
@@ -1,5 +1,8 @@
+import Button from '@material-ui/core/Button'
+import TextField, { TextFieldProps } from '@material-ui/core/TextField'
 import React, { FormEvent } from 'react'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
 import * as operations from '../state/auth/operations'
 import { State } from '../types'
 
@@ -9,6 +12,11 @@ interface Props {
   isFetchLoginStateError: boolean
 }
 
+const StyledTextField = styled(TextField)`
+  display: flex;
+  width: 15rem;
+` as React.ComponentType<TextFieldProps>
+
 class Auth extends React.Component<Props> {
   public render() {
     const { isLoginError, isFetchLoginStateError } = this.props
@@ -16,25 +24,25 @@ class Auth extends React.Component<Props> {
       <div>
         <h2>Auth</h2>
         <form onSubmit={this.handleSubmit}>
-          <ul>
-            <li>
-              <p>Username</p>
-              <p>
-                <input type="text" name="username" required />
-              </p>
-            </li>
-            <li>
-              <p>Password</p>
-              <p>
-                <input type="password" name="password" required />
-              </p>
-            </li>
-          </ul>
+          <StyledTextField
+            autoFocus
+            id="username"
+            label="Username"
+            margin="dense"
+          />
+          <StyledTextField
+            id="password"
+            label="Password"
+            margin="dense"
+            type="password"
+          />
           {isLoginError && <p>パスワードが違います</p>}
           {isFetchLoginStateError && (
             <p>セッションが切れました。もう一度ログインしてください。</p>
           )}
-          <input type="submit" value="Send" />
+          <Button variant="contained" type="submit">
+            Login
+          </Button>
         </form>
       </div>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,23 @@ import { Provider } from 'react-redux'
 import App from './containers/app'
 import configureStore from './state/store'
 
+import { createGenerateClassName, jssPreset } from '@material-ui/core/styles'
+import { create } from 'jss'
+import JssProvider from 'react-jss/lib/JssProvider'
+
+const generateClassName = createGenerateClassName()
+const jss = create({
+  ...jssPreset(),
+  insertionPoint: 'jss-insertion-point',
+})
+
 const element = document.getElementById('app')
 
 render(
   <Provider store={configureStore()}>
-    <App />
+    <JssProvider jss={jss} generateClassName={generateClassName}>
+      <App />
+    </JssProvider>
   </Provider>,
   element,
 )
