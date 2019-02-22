1b6d9093295549097416e3a169bd1f51e2893fe7
macinjoke <shunji.makino@gmail.com>
2019-02-22 19:22:09 +0900

    header container とheader components を同一ファイルに

--- src/containers/header.tsx
+++ src/containers/header.tsx
@@ -1,8 +1,53 @@
+import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button'
+import Toolbar from '@material-ui/core/Toolbar'
+import React from 'react'
 import { connect } from 'react-redux'
-import { Header } from '../components'
+import styled from 'styled-components'
 import { logout } from '../state/auth/actions'
 import { openSignUpDialog } from '../state/dialog/actions'
 import { State } from '../types'
+
+interface Props {
+  isLogin: boolean
+  logout: () => void
+  openSignUpDialog: () => void
+}
+
+const Title = styled.h1`
+  flex-grow: 1;
+`
+
+class Header extends React.Component<Props> {
+  public render() {
+    const { isLogin } = this.props
+    return (
+      <AppBar position="sticky">
+        <Toolbar>
+          <Title>elasla</Title>
+          {isLogin ? (
+            <Button onClick={this.handleLogout} color="inherit">
+              Logout
+            </Button>
+          ) : (
+            <Button color="inherit" onClick={this.handleSignUpButton}>
+              新規登録
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+    )
+  }
+  private handleLogout = () => {
+    const { logout } = this.props
+    logout()
+    localStorage.removeItem('jwt')
+  }
+  private handleSignUpButton = () => {
+    const { openSignUpDialog } = this.props
+    openSignUpDialog()
+  }
+}
 
 export default connect(
   (s: State) => ({
