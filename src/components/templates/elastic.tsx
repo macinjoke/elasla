3e65b7a2417cfc08e4647d522c473939a30e9cbd
macinjoke <shunji.makino@gmail.com>
2019-02-23 16:52:28 +0900

    elastic template にpadding

--- src/components/templates/elastic.tsx
+++ src/components/templates/elastic.tsx
@@ -1,17 +1,22 @@
 import Typography from '@material-ui/core/Typography'
 import React from 'react'
+import styled from 'styled-components'
 import SearchForm from '../organisms/searchForm'
 import SearchResultList from '../organisms/searchResultList'
+
+const Div = styled.div`
+  padding: 10px;
+`
 
 class Elastic extends React.Component<{}> {
   public render() {
     return (
-      <div>
+      <Div>
         <Typography variant="h3">Search</Typography>
         <SearchForm />
         <Typography variant="h4">Search Result</Typography>
         <SearchResultList />
-      </div>
+      </Div>
     )
   }
 }
