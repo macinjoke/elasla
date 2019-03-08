deda4ecd81f763ba65f1bb8abb8c5f85c3eb685f
macinjoke <shunji.makino@gmail.com>
2019-03-08 18:38:29 +0900

    メール認証が済んでいない人のページ追加

--- src/components/organisms/content.tsx
+++ src/components/organisms/content.tsx
@@ -1,12 +1,16 @@
+import Typography from '@material-ui/core/Typography'
 import React from 'react'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
+import { CONFIG } from '../../constants'
+import { User } from '../../state/auth/reducers'
 import { State } from '../../types'
 import Elastic from '../templates/elastic'
 import Login from '../templates/login'
 
 interface Props {
   isLogin: boolean
+  user: User
 }
 
 const Div = styled.div`
@@ -17,11 +21,31 @@ const Div = styled.div`
 
 class Content extends React.Component<Props> {
   public render() {
-    const { isLogin } = this.props
-    return <Div>{isLogin ? <Elastic /> : <Login />}</Div>
+    const { isLogin, user } = this.props
+    return (
+      <Div>
+        {isLogin ? (
+          user.isMailAuthed ? (
+            <Elastic />
+          ) : (
+            <div>
+              <Typography>
+                メール認証が済んでいません。
+                <br />
+                {user.username}@{CONFIG.mail.domain}{' '}
+                に届いているメールを確認してください。
+              </Typography>
+            </div>
+          )
+        ) : (
+          <Login />
+        )}
+      </Div>
+    )
   }
 }
 
 export default connect((s: State) => ({
   isLogin: s.auth.isLogin,
+  user: s.auth.user,
 }))(Content)
