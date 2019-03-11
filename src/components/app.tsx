0ef3de2a8c1976e93a6a09dc7ea15038e324b52a
macinjoke <shunji.makino@gmail.com>
2019-03-11 16:24:57 +0900

    fetch Session 時にjwt をlocalStorage に保存する

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -2,18 +2,21 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import React from 'react'
 import { connect } from 'react-redux'
 import * as actions from '../state/auth/operations'
+import { User } from '../state/auth/reducers'
 import Content from './organisms/content'
 import Header from './organisms/header'
 import SignUpDialog from './organisms/signUpDialog'
 
 interface Props {
-  fetchSession: () => Promise<{ username: string }>
+  fetchSession: () => Promise<User>
 }
 
 class App extends React.Component<Props> {
   public async componentWillMount() {
     const { fetchSession } = this.props
-    await fetchSession()
+    const res = await fetchSession()
+    console.log(res)
+    localStorage.setItem('jwt', res.jwt)
   }
 
   public render() {
