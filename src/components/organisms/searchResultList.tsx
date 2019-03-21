c24ac48564aa4f3aef5fe4b0973ec30754507803
macinjoke <shunji.makino@gmail.com>
2019-03-21 14:12:01 +0900

    検索結果見た目

--- src/components/organisms/searchResultList.tsx
+++ src/components/organisms/searchResultList.tsx
@@ -1,10 +1,15 @@
 import Divider from '@material-ui/core/Divider'
-import List from '@material-ui/core/List'
-import React from 'react'
+import List, { ListProps } from '@material-ui/core/List'
+import React, { ComponentType } from 'react'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
 import { Source } from '../../state/elastic/reducers'
 import { State } from '../../types'
 import SearchResult from './searchResult'
+
+const _List = styled(List)`
+  width: 94vw;
+` as ComponentType<ListProps>
 
 interface Props {
   sources: Source[]
@@ -14,14 +19,14 @@ class SearchResultList extends React.Component<Props> {
   public render() {
     const { sources } = this.props
     return (
-      <List>
+      <_List>
         {sources.map(source => (
           <>
             <SearchResult source={source} />
             <Divider />
           </>
         ))}
-      </List>
+      </_List>
     )
   }
 }
